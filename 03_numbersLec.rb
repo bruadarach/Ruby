65a3494493c6a2ed484ec55023514e5d1bcdeb87
Sujeong Ji <suji.sujeongji@gmail.com>
2019-09-22 01:57:13 +0900

    03_numbersLec.rb

---
+++ 03_numbersLec.rb
@@ -0,0 +1,30 @@
+# Number Lecture
+
+puts 1 + 1  # addition
+puts -5 + 3  # negative numbers
+puts 2.5 + 0.5 # decimal numbers
+
+puts 10 -  8 #subtraction
+puts 5 * 5 # multiplecation
+puts 5 / 2 # division # 2 # not 2.5
+
+# division
+puts 9 / 2 # 4 # not 4.5
+
+# How to see the calculation 4.5?
+puts 9 / 2.0 # make it a decimal number at least # 2.5
+puts 9.0 / 2
+puts 5 / 2.0 # 2/5
+
+# Order of Operations
+# Parentheses, Multiplication-Division, Addition-Subtraction
+puts 6 + 4 / 2 # (1) calculate 4/2 first (2) and then 6 + 2 # 8 # not 5
+puts (6 + 4) / 2 # 5
+
+# Modulo gives us the remainder of the division
+puts 13 % 5 # remainder # 3 # how? 13 / 5 = 2 remainder '3'
+puts 14 % 5 # 4
+puts 15 % 5 # 0
+
+# C.f) how to print 1 as a string?
+puts "1" # double quotation marks
