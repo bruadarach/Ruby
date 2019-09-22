deef89c63492a03cdcf8154ac804031cddd43adb
Sujeong Ji <suji.sujeongji@gmail.com>
2019-09-23 00:43:11 +0900

    05_numbersQuiz.rb

---
+++ 05_numbersQuiz.rb
@@ -0,0 +1,22 @@
+# What will 10 / 3 evaluate to?
+
+puts 10 / 3     # 3
+
+    # - Division with integers will ignore the decimal part of the result. 
+    # - If we want to get a decimal one of the numbers must be a decimal. 
+    # - For example, 10 / 3.0 will give 3.33333
+
+
+# What will 5 + 5 / 2.0 evaluate to?
+
+puts 5 + 5 / 2.0      # 7.5
+    # - Arithmetic follows the order of operations. 
+    # - Division has a higher precedence than addition.
+
+
+# What will 27 % 6 evaluate to?
+
+puts 27 % 6
+    # - Modulo (%) gives us the remainder of the divis
+    # - 27 divided by 6 is 4 w/ remainder 3. So the answer is 3.
+
