d6713d65c5fef2a81f4a0b59b66b252debdabaa3
Sujeong Ji <suji.sujeongji@gmail.com>
2019-09-23 00:42:53 +0900

    04_numberComparison.rb

---
+++ 04_numberComparison.rb
@@ -0,0 +1,20 @@
+# Ruby Number Comparison Lecture
+
+# equal ==
+# not equal !=
+# greater than >
+# less than <
+# greater than or equal to >=
+# less than or equal to <=
+
+puts 5 == 5 # true
+puts 3 == 5 # false
+
+puts 5 != 5 # false
+puts 3 != 5 # true
+
+puts 25 > 20 # true
+puts 7 < 11 # true
+
+puts 100 >= 100 # true
+puts 200 >= 100 # true 
