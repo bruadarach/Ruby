46366b8af0b84fe9fac57911a38fdd7b061cd741
Sujeong Ji <suji.sujeongji@gmail.com>
2019-10-17 22:08:42 +0900

    49_arrayEnumerableTimes.rb

---
+++ 49_arrayEnumerableTimes.rb
@@ -0,0 +1,19 @@
+
+(0...4).each { |num| puts num }
+# 0
+# 1
+# 2
+# 3
+
+(0...4).each { puts "hello" }
+# hello
+# hello
+# hello
+# hello
+
+# times : iterate a particular number of times
+4.times { puts "hi"}
+# hi
+# hi
+# hi
+# hi
