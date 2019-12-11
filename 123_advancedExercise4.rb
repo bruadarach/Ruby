59f576f88a1f5d8b804b1e1e627df7103d2da330
Sujeong Ji <suji.sujeongji@gmail.com>
2019-12-11 23:12:48 +0900

    123_advancedExercise4.rb

---
+++ 123_advancedExercise4.rb
@@ -0,0 +1,40 @@
+# Write a method, filter_lengths(strings, length), that accepts an array of strings
+# and a length as args. The method should return an array containing the strings
+# that have at least the given length. The length argument should be optional; if no length
+# is passed in, then 5 should be used as the length.
+
+def filter_lengths(strings, length=5)
+    
+    new = []
+    strings.each do |ele|
+        if ele.length >= length
+            new << ele
+        end
+    end
+    return new
+
+end
+
+p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
+p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
+p filter_lengths(["cat", "dog", "capybara", "mouse"], 7)            # => ["capybara"]
+p filter_lengths(["cat", "dog", "capybara", "mouse"])               # => ["capybara", "mouse"]
+
+
+# Teacher's solution
+def filter_lengths(strings, length=5)
+    strings.select { |str| str.length >= length }
+end 
+
+p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
+p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
+p filter_lengths(["cat", "dog", "capybara", "mouse"], 7)            # => ["capybara"]
+p filter_lengths(["cat", "dog", "capybara", "mouse"])               # => ["capybara", "mouse"]
+
+
+# Use 'select'
+nums = [1,2,3,4,5,6]
+evens = nums.select { |ele| ele % 2 == 0 } # boolean check and select 
+print evens
+puts
+# [2, 4, 6]
